99228963d4082517120f199d8f438f204ffcbefc
David Garrison <d.garrison@f5.com>
2020-08-03 15:07:01 -0700

    updated

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,27 +1,27 @@
-locals {
-    BUCKET_NAME = "${var.prefix}-consul-sd"
-    KEY_NAME = "aws_kms_key.${var.prefix}-kms-key.arn"
-    PREFIX = "${var.prefix}"
-    REGION = "${var.region}"
-}
-
-# resource "aws_kms_key" "${local.PREFIX}-kms-key" {
-#   description             = "This key is used to encrypt bucket objects"
-#   deletion_window_in_days = 7
+# locals {
+#     BUCKET_NAME = "${var.prefix}-consul-sd"
+#     KEY_NAME = "aws_kms_key.${var.prefix}-kms-key.arn"
+#     PREFIX = "${var.prefix}"
+#     REGION = "${var.region}"
 # }
 
+resource "aws_kms_key" "dgarrison-kms-key" {
+     description             = "This key is used to encrypt bucket objects"
+    deletion_window_in_days = 7
+}
+
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "${local.BUCKET_NAME}"
+  bucket = "dgarrison-consul-sd"
   acl = "private"
 
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       kms_master_key_id = "aws_kms_key.${local.KEY_NAME}"
-  #       sse_algorithm     = "aws:kms"
-  #     }
-  #   }
-  # }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "aws_kms_key.dgarrison-kms-key.arn"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 
