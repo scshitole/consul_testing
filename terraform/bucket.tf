38e55962cb8ddce4c2c36f13c8e61e2f43129eea
David Garrison <d.garrison@f5.com>
2020-08-02 22:57:09 -0700

    updated

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,27 +1,7 @@
-resource "aws_kms_key" "kms-key" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
-
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "${var.prefix}-consul-bucket"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.kms-key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
-
-
-
 terraform {
   backend "s3" {
-    bucket = "${var.prefix}-consul-bucket"
+    bucket = "dgarrison-consul-sd"
     key    = "deploy-infrastructure"
-    region = "${var.region}"
+    region = "us-east-1"
   }
 }
